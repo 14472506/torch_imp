5039c07e968a1da30b7c0c5d35de2de8c5065d88
14472506 <14472506@students.lincoln.ac.uk>
2022-04-28 15:42:35 +0100

    data loader

---
+++ data_loader.py
@@ -0,0 +1,55 @@
+import torch
+import torchvision.transforms as T
+import torch.utils.data as data
+import os 
+from pycocotools.coco import COCO
+from PIL import Image
+
+class COCOLoader(data.Dataset):
+    """
+    Custom coco data loader form pytorch
+    """
+    def __init__(self, root, json, image_transforms=None, target_transforms=None):
+        """
+        Details of init
+        """
+        self.root = root
+        self.coco = COCO(json)
+        self.ids = list(self.coco.imgs.keys())
+        self.image_transforms = image_transforms
+        self.target_transforms = target_transforms
+
+    def __getitem__(self, idx):
+        """
+        Detail Get item
+        """
+        img_id = self.ids[idx]
+        ann_ids = self.coco.getAnnIds(imgIds = img_id)
+        
+        # generating target/s
+        target = self.coco.loadAnns(ann_ids)
+
+        # loading image
+        image_path = self.coco.loadImgs(img_id)[0]['file_name']
+        img = Image.open(os.path.join(self.root, image_path)).convert('RGB')
+
+        # applying transforms
+        if self.image_transforms is not None:
+            img = self.image_transforms(img)
+
+        if self.target_transforms is not None:
+            target = self.target_transforms(target)
+
+        return img, target
+
+    def __len__(self):
+        print(len(self.ids))
+        
+if __name__ == "__main__":
+    
+    root_dir = "data/jersey_royal_ds/val"
+    json_root = "data/jersey_royal_ds/val/val.json"
+
+    img, target = COCOLoader(root_dir, json_root)
+
+    print(img)

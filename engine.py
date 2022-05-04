21231392fb293666e7ed84521e358bd2b2a4a544
Your Name <you@example.com>
2022-05-04 08:49:32 +0100

    modifying train and val from loops by adding it to epoch loop

--- engine.py
+++ engine.py
@@ -1,3 +1,5 @@
+###### imports ####################################################################################
+
 import utils
 from matplotlib import pyplot as plt
 import torch
@@ -6,7 +8,20 @@ import math
 import sys
 import json
 
+
+
+##### functions ###################################################################################
+
 def checkpoint(epoch, model, optimizer, out_dir):
+    """
+    name: checkpoint
+
+    function: The function handles the process for saving a the model
+
+    inputs:
+
+    outputs:
+    """
     # generating state checkpoint dict
     utils.mkdir(out_dir + "/checkpoints/")
     
@@ -23,8 +38,19 @@ def checkpoint(epoch, model, optimizer, out_dir):
     # saving checkpoint
     torch.save(checkpoint, file_path)
 
+
+
 def train_one_step(images, targets, model, device, optimizer, results_dict, out_dir, epoch,
-                   iter_count, metric_logger, val_freq, val_loader):
+                   iter_count, metric_logger):
+    """
+    name: training_one_step
+
+    function: The function essentially carrys out a single step of the training loop
+
+    inputs:
+
+    outputs:
+    """
     # loading images to gpu
     images = list(image.to(device) for image in images)
     targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
@@ -45,16 +71,9 @@ def train_one_step(images, targets, model, device, optimizer, results_dict, out_
     # saving model
     if not results_dict['train_loss'] or loss_value < min(results_dict['train_loss']):
         checkpoint(epoch, model, optimizer, out_dir)
+
     # appending loss value to lost list
 
-    # recordind results data
-    val_flag = iter_count/val_freq
-    if val_flag % 1 == 0:
-        with torch.no_grad():
-            val_loss_value = val_one_epoch(model, device, val_loader)
-            results_dict['val_loss'].append(val_loss_value)
-            results_dict['val_epoch'].append(iter_count)
-            model.train()
     results_dict['train_loss'].append(loss_value)
     results_dict['train_epoch'].append(iter_count)
 
@@ -66,7 +85,20 @@ def train_one_step(images, targets, model, device, optimizer, results_dict, out_
     metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
     metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
+    return results_dict
+
+
+
 def val_one_step(images, targets, model, device, loss_list):
+    """
+    name: val_one_step
+
+    function: The function carrys out a single step of the validation process
+
+    inputs:
+
+    outputs:
+    """
     # loading images to gpu
     images = list(image.to(device) for image in images)
     targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
@@ -87,8 +119,20 @@ def val_one_step(images, targets, model, device, loss_list):
     
     return loss_list
 
+
+
 def train_one_epoch(print_freq, train_loader, val_loader, model, device, optimizer, results_dict, out_dir,
                          epoch, val_freq, iter_count):
+    """
+    name: train_one_epoch
+
+    function: The function loops throught the images in an epoch passing them to the step evaluator
+              along with managing tasks at the epoch loop level such as iteration evaluation
+
+    inputs:
+
+    outputs:
+    """
     # config model for training
     model.train()
 
@@ -99,14 +143,39 @@ def train_one_epoch(print_freq, train_loader, val_loader, model, device, optimiz
     
     # iterating through images in data loader 
     for images, targets in metric_logger.log_every(train_loader, print_freq, header):
-        train_one_step(images, targets, model, device, optimizer, results_dict, out_dir, epoch,
-                   iter_count, metric_logger, val_freq, val_loader)
+        
+        # Eval model here:
+        # this must be carried out first as to not be behind the backwards call in the 
+        # train one step function.
+        val_flag = iter_count/val_freq
+        if val_flag % 1 == 0:
+            with torch.no_grad():
+                val_loss_value = val_one_epoch(model, device, val_loader)
+                results_dict['val_loss'].append(val_loss_value)
+                results_dict['val_epoch'].append(iter_count)
+                model.train()
+
+        # train one step
+        results_dict = train_one_step(images, targets, model, device, optimizer, results_dict, 
+                                      out_dir, epoch, iter_count, metric_logger)
         
         iter_count += 1
     
     return iter_count
-    
+
+
+
 def val_one_epoch(model, device, data_loader):
+    """
+    name: train_one_epoch
+
+    function: The function loops throught the images in an epoch passing them to the step evaluator
+              along with managing tasks at the epoch loop level such as iteration evaluation
+
+    inputs:
+
+    outputs:
+    """
     # config model for training
     model.train()
     loop_list = []
@@ -118,8 +187,21 @@ def val_one_epoch(model, device, data_loader):
 
     return loss_val
 
+
+
 def training_loop(model, device, optimizer, train_data_loader, val_data_loader, 
-                    start_epoch, num_epochs, print_freq, out_dir, val_freq):    
+                    start_epoch, num_epochs, print_freq, out_dir, val_freq):
+    """
+    name: train_loop
+
+    function: The main function for execution the training loop, this function loops over epochs
+              and calls all functions and sub functions, along side this the loop handles data
+              recording
+
+    inputs:
+
+    outputs:
+    """    
     # initialising data capture
     results_dict = {
         "train_loss": [],
@@ -130,6 +212,8 @@ def training_loop(model, device, optimizer, train_data_loader, val_data_loader,
      
     # epoch counter
     epoch_count = start_epoch + 1
+
+    # TO DO: maybe find a way to integrate epoch into this, or will it append to iter_count?
     iter_count = 1
 
     for epoch in range(start_epoch, num_epochs):
